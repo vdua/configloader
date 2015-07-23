19aab274c26ffd2b6db9badb061ef5912a39c343
vdua <vdua@adobe.com>
2015-07-23 09:18:43 +0530

    initial commit. setting maven project

---
+++ src/main/java/com/company/configloader/App.java
@@ -0,0 +1,13 @@
+package com.company.configloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
